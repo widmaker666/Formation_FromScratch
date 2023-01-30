c9b5cef01f960e184ba029139f4922eaa0241f93
widmaker666 <alexandre.hontcharouk69@gmail.com>
2023-01-30 13:57:39 +0100

    correction TP 3 et 4

--- Le_DOM_Javascript/Les_TP/scroll_popup/script.js
+++ Le_DOM_Javascript/Les_TP/scroll_popup/script.js
@@ -1,6 +1,6 @@
 // Créer un événement au scroll
 
-// Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
+// Réduire la navbar quand on descend vers le bas, la remettre à sa taille initiale si on remonte tout en haut
 
 // Faire apparaitre l'image de la partie improvise
 
@@ -10,16 +10,16 @@
 
 /* Reduction de la taille de la navbar qd scroll down puis remettre navbar avec la height de départ qd scroll up */
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 0) {
-    navbar.style.height = "50px";
-    navbar.style.transition = "0.5s";
+  if (window.scrollY > 50) {
+    navbar.style.height = "45px";
+    navbar.style.transition = "0.2s";
   } else {
     navbar.style.height = "90px";
   }
 });
 /* Img Improvise */
 const imgImp = document.querySelector("#imgImprovise");
-console.log(imgImp);
+
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 500) {
@@ -28,28 +28,59 @@ window.addEventListener("scroll", () => {
   } else {
     imgImp.style.opacity = "0";
   }
-  
 });
 
 /* La popup dans bas */
 const popup = document.querySelector("#popup");
-console.log(popup);
+
 window.addEventListener("scroll", () => {
-  console.log(window.scrollY);
-  if (window.scrollY > 1000) {
+  
+  if (window.scrollY > 1400) {
     popup.style.opacity = 1;
+     /* Cette écriture fonction aussi pour le tranbslate ou celle du haut */
     popup.style.transform =
-      "translateX(0)"; /* Cette écriture fonction aussi pour le tranbslate ou celle du haut */
+      "translateX(500px)";
   } else {
     popup.style.opacity = "0";
   }
 });
+
 /* avec les ID on peut directement appliquer un addeventlistner snas passer par un query selctor de l'id */
 
 closeBtn.addEventListener("click", () => {
-    popup.style.opacity = "0";
-})
+  popup.style.opacity = "0";
+});
 
-subBtn.addEventListener("click", ()=>{    
-    popup.style.opacity = "0";
-})
+subBtn.addEventListener("click", () => {
+  popup.style.opacity = "0";
+});
+ 
+
+/* Correction */
+/* 1er exo */
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.style.height = "45px";
+  } else {
+    navbar.style.height = "90px";
+  }
+
+  /* la taille total du document est : document.body.offsetHeight */
+  let scrollValue =
+    (window.scrollY + window.innerHeight) / document.body.offsetHeight;  
+
+  if (scrollValue > 0.45) {
+   imgImprovise.style.opacity = 1;
+   imgImprovise.style.transform = "none";
+  }
+
+  if (scrollValue > 0.85) {
+    popup.style.opacity = 1;
+    popup.style.transform = "none"
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+  popup.style.opacity = "0";
+  popup.style.transform = "translateX(500px)";
+});
